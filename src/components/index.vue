7847adc9b8ddbc698820706e5528f45b5f513379
sunzhaoqkd <sun.sun@ingcreations.com>
2020-08-31 14:28:38 +0800

    '格式化文件'

---
+++ src/components/index.vue
@@ -0,0 +1,27 @@
+<template>
+	<div class="serviceIndex">
+		<van-button type="primary">账号</van-button>
+		<van-button type="primary">充值</van-button>
+		<van-button type="primary">网络</van-button>
+		<van-button type="primary">bug</van-button>
+		<van-button type="primary">其他</van-button>
+		<van-button type="primary">ticket</van-button>
+	</div>
+</template>
+<script>
+	import { Button } from 'vant';
+	export default {
+		name: 'serviceIndex',
+		data() {
+			return {
+				msg: '问题反馈类型'
+			}
+		},
+		components:{
+			[Button.name]:Button
+		}
+	}
+</script>
+<style scoped>
+	
+</style>
