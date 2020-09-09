7bed9caa111e88595d706d8d249733ae4a60c716
sunzhaoqkd <sun.sun@ingcreations.com>
2020-09-09 18:03:37 +0800

    'chatdetail'

--- src/components/ticketDetail.vue
+++ src/components/ticketDetail.vue
@@ -1,6 +1,33 @@
 <template>
-	<div>
+	<div class="tickDetail">
 		<Top :backflag="true" :homeflag="true"></Top>
+		<div class="ticketdetailContain">
+			<div class="detailTitle">
+				<span>Question Type:  Others</span>
+				<span>Email: 123456@ingcreations.com</span>
+			</div>
+			<!-- 聊天记录 -->
+			<div class="chatList">
+				<!-- 时间及人物 -->
+				<div class="timeandHeros">
+					<span class="hero">
+						Me
+					</span>
+					<span class="serverTime">
+						UTC 2020/08/01 12:34:56
+					</span>
+				</div>
+				<!-- 详细内容 -->
+				<div class="">
+					
+				</div>
+			</div>
+		</div>
+
+
+
+		<div style="display:none">
+		<hr/>
 		<b>票单详情</b>
 		<pre>{{form}}</pre>
 		<ul>
@@ -29,7 +56,7 @@
 		<div v-if="!listFlag">
 			<van-button type="danger" @click="updateResolve()">问题已解决</van-button>
 			<van-button type="danger" @click="reply()">回复信息</van-button>
-		</div>
+		</div></div>
 	</div>
 </template>
 <script>
@@ -53,9 +80,9 @@
 			}
 		},
 		created(){
-			var urllist = window.location.href
-			this.ticketId = url.parse(urllist,true).query.ticketId
-			this.getRecodeList()
+			// var urllist = window.location.href
+			// this.ticketId = url.parse(urllist,true).query.ticketId
+			// this.getRecodeList()
 		},
 		methods:{
 			// 查询工单沟通记录
@@ -119,6 +146,53 @@
 		}
 	}
 </script>
-<style scoped>
-	
+<style lang="scss" scoped>
+	.tickDetail{
+		width:100%;
+		height:100%;
+		// 竖屏模式
+		@media screen and (orientation:portrait){
+
+		}
+		// 横屏模式
+		@media screen and (orientation:landscape){
+			.ticketdetailContain{
+				width:100%;
+				.detailTitle{
+					width: 100%;
+					height: 0.366rem;
+					padding:0 0.137rem;
+					box-sizing: border-box;
+					display: flex;
+					align-items: center;
+					justify-content: space-between;
+					font-size: 0.0857rem;
+					color: #405a89;
+				}
+				.chatList{
+					width:100%;
+					.timeandHeros{
+						width: 100%;
+						height: 0.228rem;
+						padding:0 0.137rem;
+						box-sizing: border-box;
+						background-color: #b3c2dc;
+						display: flex;
+						align-items: center;
+						justify-content: space-between;
+						.hero{
+							font-size: 0.091rem;
+							font-weight: bold;
+							color: #333333;
+						}
+						.serverTime{
+							font-size: 0.0857rem;
+							color: #333333;
+						}
+					}
+
+				}
+			}
+		}
+	}
 </style>
