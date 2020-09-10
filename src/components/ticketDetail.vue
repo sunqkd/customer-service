6ad76f2e053df1ca07549dbd6d66afc3b8f4d42c
sunzhaoqkd <sun.sun@ingcreations.com>
2020-09-10 12:55:58 +0800

    'update'

--- src/components/ticketDetail.vue
+++ src/components/ticketDetail.vue
@@ -2,27 +2,85 @@
 	<div class="tickDetail">
 		<Top :backflag="true" :homeflag="true"></Top>
 		<div class="ticketdetailContain">
+			<!-- 问题类型 -->
 			<div class="detailTitle">
 				<span>Question Type:  Others</span>
 				<span>Email: 123456@ingcreations.com</span>
 			</div>
 			<!-- 聊天记录 -->
-			<div class="chatList">
-				<!-- 时间及人物 -->
-				<div class="timeandHeros">
-					<span class="hero">
-						Me
-					</span>
-					<span class="serverTime">
-						UTC 2020/08/01 12:34:56
-					</span>
+			<div class="chatOutContain">
+				<div class="chatList" v-for="i in 5">
+					<!-- 时间及人物 -->
+					<div class="timeandHeros">
+						<span class="hero">
+							Me
+						</span>
+						<span class="serverTime">
+							UTC 2020/08/01 12:34:56
+						</span>
+					</div>
+					<!-- 详细内容 -->
+					<div class="chatContent">
+						<div class="chatText">
+							1. Facebook registration: open Whoot!, go to the “Login” page and tap “Facebook Login”. Whoot! will then use your Facebook account information to create your account.<br/>
+							2. Apple registration: open Whoot! Stamp, go to the “Login” page and tap “Apple Login”. Whoot! Stamp will then use your Apple account information to create your account.
+						</div>
+						<div class="chatImg">
+							<div class="chatimgContain">
+								<img src="https://img.yzcdn.cn/vant/cat.jpeg" alt="">
+							</div>
+							<div class="chatimgContain">
+								<img src="https://img.yzcdn.cn/vant/leaf.jpg" alt="">
+							</div>
+							<div class="chatimgContain">
+								<img src="https://img.yzcdn.cn/vant/tree.jpg" alt="">
+							</div>
+							<div class="chatimgContain">
+								<img src="https://img.yzcdn.cn/vant/cat.jpeg" alt="">
+							</div>
+							<div class="chatimgContain">
+								<img src="https://img.yzcdn.cn/vant/leaf.jpg" alt="">
+							</div>
+						</div>
+					</div>
 				</div>
-				<!-- 详细内容 -->
-				<div class="">
-					
+			</div>
+			<!-- 回复消息输入框 -->
+			<div>
+				
+			</div>
+			<!-- footer -->
+			<div class="replyMessage">
+				<!-- 回复信息 -->
+				<div class="replayAnswer" v-if="listFlag">
+					<span class="cancelBtn"  @click="cancelReply()">Cancel</span>
+					<span class="sendBtn" @click="sendreply()">Send</span>
+				</div>
+				<!-- 评分打星星 -->
+				<div class="replayStar" v-if="!listFlag">
+					<span class="problemBtn" @click="showScore()">problem solved</span>
+					<span class="replyBtn"  @click="reply()">Reply message</span>
 				</div>
 			</div>
 		</div>
+		<!-- 评分 弹出层-->
+		<van-overlay :show="startFlag">
+			<div class="starOut">
+				<div class="starContain">
+					<div class="starValue">
+						<van-rate v-model="startValue" :size="40" color="#d81e06" void-icon="star" void-color="#d7d8d9"/>
+					</div>
+					<div class="starText">
+						<span>Your feedback is very important,</span>
+						<span>please rate this service</span>
+					</div>
+					<div class="startBtn">
+						<span @click="cancelScore()">Cancel</span>
+						<span class="submitBtn" @click="submitScore()">Submit</span>
+					</div>
+				</div>
+			</div>
+		</van-overlay>
 
 
 
@@ -60,7 +118,7 @@
 	</div>
 </template>
 <script>
-	import { Button,Uploader,Toast } from 'vant';
+	import { Button,Uploader,Toast, Overlay, Rate} from 'vant';
 	import url from 'url'
 	import Top from '../assets/top'
 	export default {
@@ -77,6 +135,8 @@
 				],
 				listFlag:false, // 是否显示发送输入框
 				recodeList:[], // 工单记录
+				startFlag: false, // 评分弹出层
+				startValue:0,
 			}
 		},
 		created(){
@@ -85,6 +145,19 @@
 			// this.getRecodeList()
 		},
 		methods:{
+			// 显示评分
+			showScore(){
+				this.startFlag = true
+			},
+			// 取消评分
+			cancelScore(){
+				this.startFlag = false
+			},
+			// 提交评分  问题已解决
+			submitScore(){
+				this.startFlag = false;
+				// this.updateResolve();
+			},
 			// 查询工单沟通记录
 			getRecodeList(){
 				let data = {
@@ -106,7 +179,7 @@
 			reply(){
 				this.listFlag = true
 			},
-			// 取消回复
+			// 取消回复 
 			cancelReply(){
 				this.listFlag = false
 			},
@@ -142,7 +215,9 @@
 			Top,
 			[Button.name]:Button,
 			[Toast.name]:Toast,
-			[Uploader.name]:Uploader
+			[Uploader.name]:Uploader,
+			[Overlay.name]:Overlay,
+			[Rate.name]:Rate
 		}
 	}
 </script>
@@ -169,28 +244,182 @@
 					font-size: 0.0857rem;
 					color: #405a89;
 				}
-				.chatList{
-					width:100%;
-					.timeandHeros{
+				.chatOutContain{
+					width: 100%;
+					box-sizing: border-box;
+					padding-bottom: 0.5rem;
+					.chatList{
+						width:100%;
+						.timeandHeros{
+							width: 100%;
+							height: 0.228rem;
+							padding:0 0.137rem;
+							box-sizing: border-box;
+							background-color: #b3c2dc;
+							display: flex;
+							align-items: center;
+							justify-content: space-between;
+							.hero{
+								font-size: 0.091rem;
+								font-weight: bold;
+								color: #333333;
+							}
+							.serverTime{
+								font-size: 0.0857rem;
+								color: #333333;
+							}
+						}
+						.chatContent{
+							width: 100%;
+							padding: 0.11rem 0.151rem 0.137rem 0.151rem;
+							box-sizing: border-box;
+							.chatText{
+								width: 100%;
+								margin-bottom: 0.074rem;
+								color: #333333;
+								font-size: 0.0685rem;
+								line-height: 0.11rem;
+							}
+							.chatImg{
+								display: flex;
+								align-items: center;
+								flex-wrap: wrap;
+								.chatimgContain{
+									width:0.611rem;
+									height:0.611rem;
+									display: flex;
+									align-items: center;
+									justify-content: center;
+									box-sizing: border-box;
+									background-color: #ffffff;
+									border-radius: 0.0457rem;
+									border: solid 2px #000000;
+									overflow: hidden;
+									margin-right: 0.05rem;
+									margin-bottom: 0.05rem;
+								}
+								img{
+									display: block;
+									width:0.611rem;
+									height:auto;
+								}
+							}
+						}
+					}
+				}
+				.replyMessage{
+					width: 100%;
+					border-top: 1px solid #d7d8d9;
+					height: 0.37rem;
+		            position: fixed;
+					left: 0;
+					bottom: 0;
+					background:#d9e7f1;
+					font-size: 0.0857rem;
+					.replayStar{
 						width: 100%;
-						height: 0.228rem;
-						padding:0 0.137rem;
-						box-sizing: border-box;
-						background-color: #b3c2dc;
+						height: 100%;
+						display: flex;
+						align-items: center;
+						justify-content: center;
+						span{
+							display: inline-flex;
+							align-items: center;
+							height: 0.2rem;
+							padding:0 0.0657rem;
+							border: 2px solid #405a89;
+							box-sizing: border-box;
+							border-radius: 0.034rem;
+							&.problemBtn{
+								color: #405a89;
+								margin-right:0.297rem;
+							}
+							&.replyBtn{
+								background-color: #405a89;
+								color: #ffffff;
+							}
+						}
+					}
+					.replayAnswer{
+						width: 100%;
+						height: 100%;
 						display: flex;
 						align-items: center;
 						justify-content: space-between;
-						.hero{
-							font-size: 0.091rem;
-							font-weight: bold;
-							color: #333333;
-						}
-						.serverTime{
-							font-size: 0.0857rem;
-							color: #333333;
+						padding: 0 0.134rem;
+						box-sizing: border-box;
+						span{
+							display: inline-flex;
+							align-items: center;
+							&.cancelBtn{
+								color: #405a89;
+							}
+							&.sendBtn{
+								height: 0.2rem;
+								padding:0 0.0657rem;
+								border: 2px solid #405a89;
+								box-sizing: border-box;
+								border-radius: 0.034rem;
+								background-color: #405a89;
+								color: #ffffff;
+							}
 						}
 					}
-
+				}
+			}
+			.starOut{
+				display: flex;
+				align-items: center;
+				justify-content: center;
+				height: 100%;
+				width: 100%;
+				.starContain{
+					width: 1.657rem;
+					height: 0.95rem;
+					background-color: #ffffff;
+					border-radius: 0.0457rem;
+					padding-top:0.1rem;
+					box-sizing: border-box;
+					.starValue{
+						width: 100%;
+						display: flex;
+						align-items: center;
+						justify-content: center;
+						font-size: 0.183rem;
+						margin-bottom: 0.0914rem;
+					}
+					.starText{
+						display: flex;
+						flex-direction: column;
+						align-items: center;
+						justify-content: center;
+						font-size: 0.074rem;
+						color: #333333;
+						line-height:0.102rem;
+						margin-bottom: 0.0857rem;
+					}
+					.startBtn{
+						width: 100%;
+						padding:0 0.1457rem;
+						box-sizing: border-box;
+						display: flex;
+						align-items: center;
+						justify-content: space-between;
+						font-size: 0.0875rem;
+						span{
+							font-size: 0.0857rem;
+							color: #405a89;
+							&.submitBtn{
+								display: inline-flex;
+								align-items: center;
+								height:0.2rem;
+								padding:0 0.1514rem;
+								color: #ffffff;
+								background-color: #405a89;
+								border-radius: 0.0457rem;
+							}
+						}
+					}
 				}
 			}
 		}
